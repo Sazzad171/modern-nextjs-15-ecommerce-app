a8e373443cf114e4158529aabf66f9b0147eb522
sazzad.jafor@technonext.com <sazzad.jafor@technonext.com>
2026-01-05 00:13:00 +0600

    feat: home site details added

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -216,6 +216,29 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      {/* site description */}
+      <section className="bg-gray-50 py-8">
+        <div className="container mx-auto px-4">
+          <h2 className="mb-4 text-2xl font-semibold">
+            Leading Computer, Laptop & Gaming PC Retail & Online Shop in Bangladesh
+          </h2>
+          <p className="text-gray-700">
+            Technology has become a part of our daily lives, and we depend on tech products daily
+            for a vast portion of our lives. There is hardly a home in Bangladesh without a tech
+            product. This is where we come in. Star Tech Ltd. started as a Tech Product Shop in
+            March 2007. We focus on giving the best customer service in Bangladesh, following our
+            motto of “Customer Comes First.” This is why Star Tech is the most trusted computer shop
+            in Bangladesh today, capturing the loyalty of a large customer base. After a long
+            16-year journey, Star Tech Ltd. was certified with the renowned "ISO 9001:2015
+            certification" as a recognition for the best Quality Control Management System. As an
+            ISO-certified organization, Star Tech Ltd. is now up to the international standards that
+            specify a Quality Management System (QMS). This Certification denotes that the
+            organization strictly maintains all sorts of regulatory requirements to provide
+            customers with products and services of a global standard.
+          </p>
+        </div>
+      </section>
     </>
   );
 }
